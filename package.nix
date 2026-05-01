1605f12367023ce7f4e11d2fcb15ee9dbbfd1cc5
Ratakor <ratakor@disroot.org>
2026-05-01 23:59:40 +0200

    Add package.nix
    Signed-off-by: Ratakor <ratakor@disroot.org>

---
+++ package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenvNoCC,
+  categories ? [ ], # leave empty for all
+  extensions ? [ ], # leave empty for all, available: jpeg jpg png webp
+}:
+let
+  allCategories = lib.filterAttrs (name: type: type == "directory" && !(lib.hasPrefix "." name)) (
+    builtins.readDir ./.
+  );
+
+  selectedCategories = if categories == [ ] then builtins.attrNames allCategories else categories;
+
+  invalidCategories = builtins.filter (
+    category: !(builtins.hasAttr category allCategories)
+  ) selectedCategories;
+
+  extFilter =
+    if extensions == [ ] then
+      ""
+    else
+      "\\( ${lib.concatMapStringsSep " -o " (ext: "-name '*.${ext}'") extensions} \\)";
+in
+if invalidCategories != [ ] then
+  throw "Invalid categories: ${lib.concatStringsSep ", " invalidCategories}"
+else
+  stdenvNoCC.mkDerivation {
+    pname = "wallpapers";
+    version = "1.0.0";
+
+    src = ./.;
+
+    installPhase = ''
+      mkdir -p $out
+    ''
+    + lib.concatMapStringsSep "\n" (
+      category:
+      "find '${category}' -maxdepth 1 -type f ${extFilter} ! -name 'README.md' -exec cp -t $out/ {} \\;"
+    ) selectedCategories;
+  }
