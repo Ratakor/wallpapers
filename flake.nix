37e557a623f3ec9604fca90a26b67c12b51171cf
Ratakor <ratakor@disroot.org>
2026-07-15 23:24:53 +0200

    replace nixpkgs overlay with package
    Signed-off-by: Ratakor <ratakor@disroot.org>

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
+  inputs.nixpkgs.url = "nixpkgs";
+
   outputs =
-    { self, ... }:
+    { self, nixpkgs, ... }:
     let
+      forAllSystems = f: builtins.mapAttrs (_system: pkgs: f pkgs) nixpkgs.legacyPackages;
+
       date = builtins.concatStringsSep "-" (builtins.match "(.{4})(.{2})(.{2}).*" self.lastModifiedDate);
       version = "0-unstable-${date}";
     in
     {
-      overlays.default = _final: pkgs: {
-        wallpapers = pkgs.callPackage ./package.nix { inherit version; };
-      };
+      packages = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./package.nix { inherit version; };
+      });
     };
 }
